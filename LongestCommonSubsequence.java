130fd1362d0a948f753fa83dd107fd67b2d58d03
Bhargav-suman <108222253+Bhargav-suman@users.noreply.github.com>
2024-04-10 06:40:58 +0530

    Create LongestCommonSubsequence.java

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,44 @@
+public class LongestCommonSubsequence {
+    public static String findLCS(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Building the dp array
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Constructing the LCS string
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                lcs.insert(0, str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.toString();
+    }
+
+    public static void main(String[] args) {
+        String str1 = "AGGTAB";
+        String str2 = "GXTXAYB";
+        System.out.println("Longest Common Subsequence: " + findLCS(str1, str2));
+    }
+}
